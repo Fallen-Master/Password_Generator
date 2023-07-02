ca22a1eaf94c934dfc78329aec5a81b5abdef323
Rafael Gomez <rgomez2014@gmail.com>
2023-07-01 20:02:24 -0500

    added comments to js

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,27 @@
 // Assignment Code
+//make a function for generatePassword
+
+
+
+
+
+
+
+//Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
+
+  //We will write a finchion called generatePassword that will return a string. this string will be assigned to the password var
+  var password = generatePassword();
+
+  // get reference to password dispay text
+  //references to password text
+  var passwordText = document.querySelector("#password"); 
+
+  // change the value of the password display text to the string assigned to our password variable
   passwordText.value = password;
 
 }
