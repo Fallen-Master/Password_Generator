1a2412e2253322b4e4db793c6e81ef7cde9cd4b8
Rafael Gomez <rgomez2014@gmail.com>
2023-07-04 20:16:36 -0500

    a lot

--- Assets/script.js
+++ Assets/script.js
@@ -1,91 +1,88 @@
-// Assignment Code
-//make a function for generatePassword
-  var lowercase = [," a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var uppercase = [," A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var numbers = [," 1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  var specialChar = [ ," !", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]","\\", "^", "_", "{", "}", "~"];
-
-
+var yes = true
+var no = false
+var empty = ""
 
 function generatePassword() {
   var length = window.prompt("Password must be between 8 to 128 characters long")
-
+  
   length = length * 1;
 
   if (isNaN(length) || length < 8 || length > 128){
-    alert("That is not a number...")
+    alert("Please choose a length between 8 and 128")
     return;
   };
- 
-  var uppercasePrompt = window.confirm("Would you like to have uppercase characters?");
-
-  var lowercasePrompt = window.confirm("Would you like to have lowercase characters?");
-
-  var numbersPrompt = window.confirm("Would you like to have number characters?");
-
-  var specialCharPrompt = window.confirm("Would you like to have special characters?" );
-
-
-  if(!uppercasePrompt && !lowercasePrompt && !numbersPrompt && !specialCharPrompt){
-    alert("Please confirm on of the options")
+  var uppercasePrompt = window.prompt ("Would you like to have uppercase characters yes or no?")
+  if(uppercasePrompt.toLowerCase() !=="yes" && uppercasePrompt.toLowerCase()!=="no" ) {
+    alert("Please type in yes or no")
     return;
   }
 
-  var combine = ""
+  var lowercasePrompt = window.prompt ("Would you like to have lowercase characters yes or no?")
+  if(lowercasePrompt.toLowerCase() !=="yes" && lowercasePrompt.toLowerCase()!=="no" ) {
+    alert("Please type in yes or no")
+    return;
+  }
 
-  if (uppercasePrompt == true) {
-    combine = combine.concat(uppercase)
+  var numbersPrompt = window.prompt ("Would you like to have number characters yes or no?")
+  if(numbersPrompt.toLowerCase() !=="yes" && numbersPrompt.toLowerCase()!=="no" ) {
+    alert("Please type in yes or no")
+    return;
+  }
+
+  var specialCharPrompt = window.prompt ("Would you like to have number characters yes or no?")
+  if(specialCharPrompt.toLowerCase() !=="yes" && specialCharPrompt.toLowerCase()!=="no" ) {
+    alert("Please type in yes or no")
+    return;
+  }
+
+  if(uppercasePrompt.toLowerCase() ==="no" && lowercasePrompt.toLowerCase() ==="no" && numbersPrompt.toLowerCase() ==="no" && specialCharPrompt.toLowerCase() ==="no"){
+    alert("Please choose one of the characters")
+    return empty;
+
+  }
+
+
+  let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  let specialChar = [ "!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]","\\", "^", "_", "{", "}", "~"];
+
+  var combine = [];
+
+  if (uppercasePrompt.toLowerCase() === "yes") {
+    combine = combine.concat(uppercase);
   }
   
-  if (lowercasePrompt == true){
-    combine = combine.concat(lowercase)
+  if (lowercasePrompt.toLowerCase() === "yes"){
+    combine = combine.concat(lowercase);
   }
 
-  if(numbersPrompt == true){
-    combine = combine.concat(numbers)
+  if(numbersPrompt.toLowerCase() === "yes"){
+    combine = combine.concat(numbers);
   }
 
-  if(specialCharPrompt == true){
-    combine = combine.concat(specialChar)
+  if(specialCharPrompt.toLowerCase() === "yes"){
+    combine = combine.concat(specialChar);
   }
+
+ var password = ""
 
  for ( var i = 0; i < length; i++){
-  var index = Math.floor(Math.random() * combine.length)
-  
+  var index = Math.floor(Math.random() * combine.length);
+ password += combine[index]
  }
-console.dir(combine)
-console.dir(combine.length)
 
+return password;
 }
 
-
-//Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password"); 
   passwordText.value = password;
-
+ 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Password_Generator
-This application will generate passwords with the criteria you have chosen 
+
+## Description
+The Password Generator application is designed for individuals who tend to reuse the same passwords repeatedly. This tool allows users to generate unique and secure passwords by specifying the desired length and selecting from various character types. For instance, with a password length of eight characters and using all character types, it is theoretically possible to generate approximately 7.96 trillion unique passwords.
+___
+## Features
+- Password length can be any where from 8-128 characters
+- All the UPPERCASE characters of the alphabet are available
+- All the lowercase characters of the alphabet are available
+- All the special characters you find on a typical keyboard are also available
+
+- All numbers from 0-9 are available 
+
+___
+## Preview
+![image](./Assets/Password_Generator_test.jpg)
+___
+### Credit
+The Staff at Bootcamp Spot for creating the HTML, CSS and, starting the JavaScript.   
+[Rafael Gomez](https://github.com/Fallen-Master) made additional changes to the exsiting JavaScript.
+___
+### License
+This project is licensed under the [MIT License](LICENSE).
